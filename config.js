d1f840392a78d836b0a0c881f419585a85b66992
Issadeen <75924084+Issadeen@users.noreply.github.com>
2024-04-21 22:32:24 +0300

    oilwell 1

---
+++ config.js
@@ -0,0 +1,10 @@
+// config.js
+const firebaseConfig = {
+    apiKey: "AIzaSyDklv5Sgwiy84_fCOqiQRCiuTBVRZ_2EvY",
+    authDomain: "my-1-wb.firebaseapp.com",
+        projectId: "my-1-wb",
+        storageBucket: "my-1-wb.appspot.com",
+        messagingSenderId: "865633557898",
+        appId: "1:865633557898:web:5e8a848b08d7475054bc73"
+  };
+  
